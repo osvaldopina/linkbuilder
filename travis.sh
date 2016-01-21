248acaac7194d9aa872fe4e9de05f5084b1cb57c
osvaldo.pina <osvaldo.pina@gmail.com>
2016-01-21 08:37:22 -0200

    working on travis-ci config

---
+++ travis.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+
+#
+#
+#  Check, in pom.xml, if current version is
+#  a snapshot or not
+#
+#
+isSnapshotVersion() {
+  if grep -q "<version>.*-SNAPSHOT</version>" pom.xml
+  then
+      return 0;
+  else
+      return 1;
+  fi
+}
+
+#
+#
+#  Check, in pom.xml, if current version is
+#  a final versio or not
+#
+#
+isFinalVersion() {
+  if isSnapshotVersion
+  then
+    return 1;
+  else
+    return 0;
+  fi
+}
+
+#
+#
+#  Check if travis is trying to build a
+#  pull request or not
+#
+#
+isPullRequest() {
+  if [ "${TRAVIS_PULL_REQUEST}" == "false" ]
+  then
+    return 1;
+  else
+    return 0;
+  fi
+}
+
+if isPullRequest
+then
+    if isFinalVersion
+    then
+        echo "Pull Request should never have final versions in pom.xml!!!!"
+        exit -1
+    else
+        echo "executing tests for pull request ${TRAVIS_PULL_REQUEST}..."
+        mvn test --settings travis-ci-maven-settings.xml
+    fi
+elif isSnapshotVersion
+then
+  echo "deploying snapshot..."
+  mvn deploy --settings travis-ci-maven-settings.xml
+else
+  echo "executing tests for a final version..."
+  mvn test --settings travis-ci-maven-settings.xml
+fi
